1ddf10fa878840c7085c70e71362752e166a4a4e
Arthur Soares <148705578+arthursoares2509@users.noreply.github.com>
2026-05-29 00:27:22 -0300

    Criar summation.c

---
+++ Math Codes/summation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 0; //acc
+    int i;
+
+    //loop for summation
+    for (i = 1; i <= 100; i++) {
+        sum += 1; //sum + i
+
+}
+
+
+      printf("Summation Result: %d\n", sum); 
+
+      return 0;
+}
